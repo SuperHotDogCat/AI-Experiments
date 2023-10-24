9e842b3d7b0f3cd8a7ae7b9886ebf9ff985f9968
SuperHotDogCat <siromisochan@gmail.com>
2023-10-24 15:55:49 +0900

    自然言語処理追加

---
+++ experiment/day2/nlp/translator_model_test_full.py
@@ -0,0 +1,39 @@
+import sys
+import torch
+
+import sentence_data
+from translator_model import TranslatorModel
+
+dataset = sentence_data.SentenceData("dataset/data_full.txt")
+
+model = TranslatorModel(dataset.english_word_size(),
+                        dataset.japanese_word_size())
+
+model.load_state_dict(torch.load("trained_model/translator_full.model"))
+"""
+未知語対応のため一部書き換えた
+"""
+# 入力された文章を単語に分割する
+sentence = input("input an english sentence : ").split(' ')
+# 単語IDのリストに変換する
+sentence_id = []
+for word in sentence:
+    if not word:
+        # 単語が空だったら飛ばす
+        continue
+    word = word.lower()
+    id = dataset.english_word_id(word)
+    if id is None:
+        id = torch.tensor(1,dtype=torch.long).unsqueeze(-1)
+        sentence_id.append(id)
+        #print(dataset.japanese_word(id)) <- しっかり<UNKNOWN>がでていることがわかる
+        #sys.stderr.write("Error : Unknown word " + word + "\n")
+        #sys.exit()
+    else:
+        id = torch.tensor(id,dtype=torch.long).unsqueeze(-1)
+        sentence_id.append(id)
+
+japanese = model(torch.stack(sentence_id))
+for id in japanese:
+    print(dataset.japanese_word(id), end='')
+print()
